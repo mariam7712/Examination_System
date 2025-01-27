af92c5578f4284e1105c0c182ccc15a56e73fd41
banseh <99220556+banseh@users.noreply.github.com>
2025-01-27 18:25:35 +0200

    Update succed.js

--- succed/succed.js
+++ succed/succed.js
@@ -1,3 +1,26 @@
+$("#display").hide();
+$("#imgsucced").hide();
+$("#btn1").hide();
+
+$("#bg").animate(
+  {
+    width: "100%",
+  },
+  5000,
+  "linear",
+  function () {
+    $("#bg").fadeOut(500, function () {
+      $(this).remove();
+    });
+    $("#imgsucced").fadeIn(500, function () {
+      $("#display").fadeIn(500, function () {
+        $("#btn1").fadeIn(500);
+      });
+    });
+  }
+);
+
+//get element from local storage
 const firstName = window.localStorage.getItem("firstName");
 const score = window.localStorage.getItem("finalgrade");
 const display = $("#display");
