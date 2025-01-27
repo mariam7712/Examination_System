6e94c34ef4a5ac07083a186cab3d86c97abb555a
banseh <99220556+banseh@users.noreply.github.com>
2025-01-27 17:53:26 +0200

    Update fail.js

--- fail/fail.js
+++ fail/fail.js
@@ -1,3 +1,31 @@
+
+      $("#display").hide();
+      $("#imgfail").hide();
+      $("#btn1").hide();
+      $("#btn2").hide();
+
+      $("#bg").animate(
+        {
+          width: "100%",
+        },
+        5000,
+        "linear",
+        function () {
+          $("#bg").fadeOut(1000, function () {
+            $(this).remove(); // Remove the #bg element after fadeOut
+          });
+          $("#imgfail").fadeIn(1000, function () {
+            $("#text").fadeIn(1000, function () {
+              $("#btn1").fadeIn(1000);
+              $("#btn2").fadeIn(1000);
+            });
+          });
+        }
+      );
+      console.log("hello");
+
+
+// /////////////////////////////////////////////
 const firstName = window.localStorage.getItem("firstName");
 const score = window.localStorage.getItem("finalgrade");
 const display = $("#display");
